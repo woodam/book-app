47412a1db88a42a6dc59b5be6401c381a6c24b05
ji man, lee <ace4gi@nate.com>
2014-03-03 16:38:33 +0900

    update

---
+++ chapter-backbone/todoMVC.marionette(requirejs)/js/main.js
@@ -0,0 +1,36 @@
+require.config({
+  paths : {
+    underscore : 'lib/underscore',
+    backbone   : 'lib/backbone',
+    marionette : 'lib/backbone.marionette',
+    jquery     : '../../js/jquery',
+    tpl        : 'lib/tpl'
+  },
+  shim : {
+    'lib/backbone-localStorage' : ['backbone'],
+    underscore : {
+      exports : '_'
+    },
+    backbone : {
+      exports : 'Backbone',
+      deps : ['jquery','underscore']
+    },
+    marionette : {
+      exports : 'Backbone.Marionette',
+      deps : ['backbone']
+    }
+  },
+  deps : ['jquery','underscore']
+});
+
+require(['app','backbone','routers/index','controllers/index'],function(app,Backbone,Router,Controller){
+  "use strict";
+
+  app.start();
+  new Router({
+    controller : Controller
+  });
+  Backbone.history.start();
+});
+
+
